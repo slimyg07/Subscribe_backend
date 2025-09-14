a422275ca86569fcf94cb43e43f2076bdeafba78
Harshvardhan Dixit <kacked07@gmail.com>
2025-09-14 23:35:32 +0530

    User fetch logic added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -124,3 +124,26 @@ export const getUser = async (req , res, next) => {
     }
 }
 
+
+export const getSingleuser = async (req, res, next) => {
+
+    try {
+        
+        const user = await User.findById(req.params.id).select('-password');
+
+        if(!user){
+            const error = new Error("Invalid ID");
+            error.status = 404;
+            throw error;
+        }
+
+        res.status(200).json({
+            success : true,
+            message : "User details",
+            data : user
+        })
+        
+    } catch (error) {
+        next(error);
+    }
+}
